8d15c78e80c07810d0525ebb6fcdf8772e9a925f
Olivier Dusabimana <diokeyolivier@gmail.com>
2016-04-03 11:33:22 -0400

    CSV & XLS Download

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var fs = require('fs');
 var db = require('../controllers/db');
 var json2csv = require('json2csv');
+var excelbuilder = require('msexcel-builder');
 
 var getQuery = function (options) {
   var fields = options.fields;
@@ -106,20 +107,9 @@ router.post('/results', function (req, res) {
 
 });
 
-router.post('/download', function (req, res) {
-  var selectedFields= req.body.fields;
-  var selectedLocations = req.body.locations;
-  var selectedSources = req.body.sources;
-
-  var queries = {
-    'fields': selectedFields.split(','),
-    'locations': selectedLocations.split(','),
-    'sources': selectedSources.split(',')
-  };
-
-  var sqlQuery = getQuery(queries);
-
+var createCSV = (sqlQuery, callback) => {
   db.query(sqlQuery).then(function (result) {
+    // downloadCsv(result, fields, res);
     var fields = ['report_date', 'data_field_name', 'value', 'data_field_unit', 'location_name', 'source_name'];
 
     json2csv({ data: result, fields: fields }, function(err, csv) {
@@ -130,14 +120,85 @@ router.post('/download', function (req, res) {
 
       fs.writeFile('mycsv.csv', csv, function (err) {
         if (!err) {
-          res.download('mycsv.csv');
+          callback(null, 'mycsv.csv');
         } else {
           console.log(err);
-          res.redirect('/');
+          callback(err, null);
         }
       });
     });
   });
+};
+
+var createExcel = (sqlQuery, callback) => {
+  var cols = ['report_date', 'data_field_name', 'value', 'data_field_unit', 'location_name', 'source_name'];
+  var workbook = excelbuilder.createWorkbook('./', 'myxls.xlsx');
+  console.log(sqlQuery);
+  db.query(sqlQuery).then(function (data) {
+    //titles
+    var sheet1 = workbook.createSheet('sheet1', cols.length, data.length);
+    cols.forEach((col, c) => {
+      sheet1.set(c+1, 1, col);
+    });
+
+    data.forEach((row,i) => {
+      cols.forEach((col, j) => {
+        try {
+          sheet1.set(j+1, i+2, row[col]);
+        } catch(e) {
+          console.log(e);
+        }
+      });
+    });
+
+    workbook.save(function (ok) {
+      if (!ok) {
+        callback(null, 'myxls.xlsx');
+      } else {
+        callback(new Error('Unable to create XLSX'), null);
+      }
+    });
+  })
+  .catch(e=> callback(e, null));
+
+
+};
+
+router.post('/download', function (req, res) {
+  var selectedFields= req.body.fields;
+  var selectedLocations = req.body.locations;
+  var selectedSources = req.body.sources;
+  var file_type = req.body.file_type;
+
+  var queries = {
+    'fields': !selectedFields ? null : selectedFields.split(','),
+    'locations': !selectedLocations? null : selectedLocations.split(','),
+    'sources': !selectedSources? null : selectedSources.split(',')
+  };
+
+  var sqlQuery = getQuery(queries);
+
+  console.log('File type', file_type);
+  if (file_type === 'xlsx') {
+    createExcel(sqlQuery, function (err, file_name) {
+      if (!err) {
+        res.download(file_name);
+      } else {
+        console.log(err);
+        res.redirect('/');
+      }
+    });
+
+  } else {
+    createCSV(sqlQuery, function (err, file_name) {
+      if (!err) {
+        res.download(file_name);
+      } else {
+        console.log(err);
+        res.redirect('/');
+      }
+    });
+  }
 
 });
 
